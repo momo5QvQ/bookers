1a4dd430b51280043e0de44de0330c898232a8a5
momo5QvQ <132321554+momo5QvQ@users.noreply.github.com>
2023-05-12 20:25:32 +0000

    [update]新規投稿ページ

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
-  get 'books/create'
-  get 'books/index'
-  get 'books/show'
-  get 'books/edit'
+  get 'books/new'
+  post 'books' =>'books#new'
+  post 'books' =>'books#create'
+  get 'books' =>'books#index'
+  get 'books' =>'books#show'
+  get 'books' =>'books#edit'
   get 'homes/top'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
